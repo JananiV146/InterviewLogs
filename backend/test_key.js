03abcbe28e936b30da5a1a9434e74ac1046c2e6a
JananiV1406 <jananiv.23aid@kongu.edu>
2026-05-20 11:46:20 +0530

    Second commit

--- backend/test_key.js
+++ backend/test_key.js
@@ -1,7 +1,8 @@
+import 'dotenv/config.js';
 import fetch from 'node-fetch';
 
 async function test() {
-  const key = 'AIzaSyAsLUz3a3y_JIYhMmw_FJ7YblkrBGvZvvM';
+  const key = process.env.GEMINI_API_KEY;
   const url = `https://generativelanguage.googleapis.com/v1beta/models?key=${key}`;
   const res = await fetch(url);
   const data = await res.json();
